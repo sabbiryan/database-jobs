a0e05acc6cccbc064a6526d55c02c7d5582510a1
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2020-05-17 02:34:55 +0600

    schedule task register package added

--- Source/DbBackupScheduler/DbBackup.NetFramework/Program.cs
+++ Source/DbBackupScheduler/DbBackup.NetFramework/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using DbBackup.Shared;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
+using Microsoft.Win32.TaskScheduler;
 using Serilog;
 using Serilog.Core;
 
@@ -25,6 +26,28 @@ namespace DbBackup
             List<Database> databases = DatabaseProvider.GetDatabasesToBackup();
 
             BackupBuilder.GenerateBackups(server, databases);
+
+
+
+            //// Get the service on the local machine
+            //using (TaskService ts = new TaskService())
+            //{
+            //    // Create a new task definition and assign properties
+            //    TaskDefinition td = ts.NewTask();
+            //    td.RegistrationInfo.Description = "Does something";
+
+            //    // Create a trigger that will fire the task at this time every other day
+            //    td.Triggers.Add(new DailyTrigger { DaysInterval = 2 });
+
+            //    // Create an action that will launch Notepad whenever the trigger fires
+            //    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+            //    // Register the task in the root folder
+            //    ts.RootFolder.RegisterTaskDefinition(@"Test", td);
+
+            //    // Remove the task we just created
+            //    ts.RootFolder.DeleteTask("Test");
+            //}
         }
 
 
