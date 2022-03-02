cf660c95d093662644ec98c461b7b915347abf1c
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2022-03-02 09:03:10 +0600

    nupkg class lib added and build first nuget pkg

---
+++ Source/DatabaseJobs/DatabaseJobs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseJobs.Backup;
+using DatabaseJobs.Maintenance;
+using DatabaseJobs.Shared;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace DatabaseJobs
+{
+    public class DatabaseJobs
+    {
+        public void Run()
+        {
+            Server server = ServerConnector.Connect();
+
+            List<Database> databases = DatabaseProvider.GetDatabasesToBackup(server);
+
+            List<string> connectionStrings = DatabaseProvider.GetConnectionStrings(server);
+
+            IndexOrganizer.Reorganize(connectionStrings);
+
+            BackupBuilder.GenerateBackups(server, databases);
+
+            BackupCleaner.CleanAllBackups();
+        }
+
+    }
+}
---
+++ Source/DatabaseJobs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DatabaseJobs")]
+[assembly: AssemblyDescription("Taking backups, index rebuild or reorganize, push backups to azure storage or aws s3. A complete database maintenance jobs package.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("datacrud")]
+[assembly: AssemblyProduct("DatabaseJobs")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe27f65e-351b-44f0-ac5e-cd27d7f71d77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
